ced413cfe08d9098dba98d453b01f89d96a9a8c7
kartik11m <kartikmaheshwari698@gmail.com>
2025-11-02 01:34:37 +0530

    created the authentication middleware using jwt for identifying the correct user on the server

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+const userModel = require('../models/user.model');   
+const bcrypt = require('bcrypt');     
+const jwt = require('jsonwebtoken');
+const blacklistTokenModel = require('../models/blacklistToken.model');
+
+module.exports.authUser = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
+
+    const isBlacklisted = await userModel.findOne({token: token});
+
+    if(isBlacklisted) {
+        return res.status(401).json({ message: 'Token is blacklisted.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded._id);
+        
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid token.' });
+        }
+
+        req.user = user;
+        return next();
+        
+    } catch (error) {
+        return res.status(400).json({ message: 'Invalid token.' });
+    }
+}
